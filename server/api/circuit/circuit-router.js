fadbfce1ca6631ee936baaf815a8422db5c63d6e
alphaseinor <alphaseinor@gmail.com>
2020-12-27 23:11:03 -0600

    made LED an idempotent list, made a blinking routine for flash, still need to implement flash on all circuits

--- server/api/circuit/circuit-router.js
+++ server/api/circuit/circuit-router.js
@@ -7,39 +7,62 @@ let state = {
 }
 
 
-let LED = [
-    new Gpio(4, 'out'),
-    new Gpio(17, 'out'),
-    new Gpio(18, 'out'),
-    new Gpio(27, 'out'),
-    new Gpio(22, 'out'),
-    new Gpio(23, 'out'),
-    new Gpio(24, 'out'),
-    new Gpio(25, 'out'),
-    new Gpio(5, 'out'),
-    new Gpio(6, 'out'),
-    new Gpio(12, 'out'),
-    new Gpio(13, 'out'),
-    new Gpio(19, 'out'),
-    new Gpio(16, 'out'),
-    new Gpio(26, 'out'),
-    new Gpio(20, 'out')
-]
+// let LED = [
+    // new Gpio(4, 'out'),
+    // new Gpio(17, 'out'),
+    // new Gpio(18, 'out'),
+    // new Gpio(27, 'out'),
+    // new Gpio(22, 'out'),
+    // new Gpio(23, 'out'),
+    // new Gpio(24, 'out'),
+    // new Gpio(25, 'out'),
+    // new Gpio(5, 'out'),
+    // new Gpio(6, 'out'),
+    // new Gpio(12, 'out'),
+    // new Gpio(13, 'out'),
+    // new Gpio(19, 'out'),
+    // new Gpio(16, 'out'),
+    // new Gpio(26, 'out'),
+    // new Gpio(20, 'out')
+// ]
+
+let LED = {
+    0: 4,
+    1: 17,
+    2: 18,
+    3: 27,
+    4: 22,
+    5: 23,
+    6: 24,
+    7: 25,
+    8: 5,
+    9: 6,
+    10: 12,
+    11: 13,
+    12: 19,
+    13: 16,
+    14: 26,
+    15: 20
+}
+
+const initGPIO = () => {
+    for(let i = 0; i < 16; i++){
+        if(state.circuit[i].gpio){
+            state.circuit[i].gpio.writeSync(0)
+        }
+    }
+}
 
 //initialize 16 circuit states to off or disabled
-let initState = () => {
+let initState = async () => {
     for(let i = 15; i >= 0; i--){
-        //{
-        //     error: "none",
-        //     state: "disabled"
-        // }
-        LED[i].writeSync(0)
         if(state.coding - Math.pow(2,i) >= 0){
             state.coding = state.coding - Math.pow(2,i)
             state.circuit = [...state.circuit, 
                 {
                     error: "none",
-                    state: "off"
+                    state: "off",
+                    gpio: new Gpio(LED[i + ""], 'out')
                 }
             ]
         } else {
@@ -50,9 +73,9 @@ let initState = () => {
                 }
             ]
         }
-
     }
-    return true
+    return await initGPIO()
+    
 }
 
 initState()
@@ -62,12 +85,19 @@ state.coding = parseInt(process.env.CODING)
 console.log(state)
 
 
+
+
 const checkBody = (req, res, next) => {
     if(!req.body.state){
         console.log("checkBody: missing state in body")
-        res.status(500).json({error : "missing state in body",  circuit: id})
+        res.status(500).json({error : "missing state in body",  circuit: req.params.id})
     }else{
-        next()
+        if(req.params.id < 16 || req.params.id > -1){
+            next()
+        }else {
+            console.log("checkBody: ID does not exist")
+            res.status(500).json({error : "ID does not exist",  circuit: req.params.id})
+        }
     }
 }
 
@@ -80,25 +110,44 @@ const checkDisabled= (req, res, next) => {
         res.status(500).json({error : `State is disabled on circuit ${id}, you must recode module to allow this output to be used`})
     }else{
         next()
-    }
-    
+    }    
 }
+
+
+let blinkInterval = setInterval(blinkLED, 500);
+
+function blinkLED() {
+    if(state.circuit[2].state == "on"){
+        if (state.circuit[2].gpio.readSync() === 0) {
+            state.circuit[2].gpio.writeSync(1)
+        } else {
+            state.circuit[2].gpio.writeSync(0)
+        }
+    }
+}
+
+// not needed because we will use this the whole time it's up
+// function endBlink(id) {
+//   clearInterval(blinkInterval)
+//   LED[2].writeSync(0)
+//   LED[2].unexport()
+// }
 
 router.post('/reset', async (req,res) => {
     await initState()
     res.status(200).json({error: "none", state})
 })
 
-router.post('/:id', checkDisabled, checkBody, async (req, res) =>{
+router.post('/:id', checkBody, checkDisabled, async (req, res) =>{
     const id = req.params.id
     if(req.body.state == "on"){
         console.log(`circuit ${id} is now on`)
-        LED[id].writeSync(1)
+        state.circuit[id].gpio.writeSync(1)
         state.circuit[id].state = "on"
         res.status(200).json({error: "none", circuit: id, state: "on"})
     }else{
         console.log(`circuit ${id} is now off`)
-        LED[id].writeSync(0)
+        state.circuit[id].gpio.writeSync(0)
         state.circuit[id].state = "off"
         res.status(200).json({error: "none", circuit: id, state: "off"})
     }
