f06ff318bb365c873d3aae0be621e17a3541adad
alphaseinor <alphaseinor@gmail.com>
2020-12-27 19:56:28 -0600

    added modified files in parent folders

--- server/api/server.js
+++ server/api/server.js
@@ -3,49 +3,16 @@ require('dotenv').config();
 
 const server = express();
 
+const circuitRouter = require('./circuit/circuit-router').router
+
 server.use(express.json());
 
-let state = {
-    coding: parseInt(process.env.CODING),
-    circuit: [] 
-}
-
-
-
-//initialize 16 circuit states to off or disabled
-for(let i = 16; i >= 0; i--){
-    //{
-    //     error: "none",
-    //     state: "disabled"
-    // }
-    if(state.coding - Math.pow(2,i) >= 0){
-        state.coding = state.coding - Math.pow(2,i)
-        state.circuit = [...state.circuit, 
-            {
-                error: "none",
-                state: "off"
-            }
-        ]
-    } else {
-        state.circuit = [...state.circuit, 
-            {
-                error: "none",
-                state: "disabled"
-            }
-        ]
-    }
-
-}
-
-state.circuit = state.circuit.reverse()
-state.coding = parseInt(process.env.CODING)
-console.log(state)
-
-
+server.use('/circuit', circuitRouter)
 
 server.use('/', (req, res)=>{
   res.status(200).json({error: false, state: state})
 })
 
 
-module.exports = server;
+exports.server = server
+
